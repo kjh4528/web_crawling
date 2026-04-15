f94df74916325d9f39f6c924d061edf9dd04efab
kjh4528 <jeonghyeon.kimm@gmail.com>
2026-04-15 22:25:20 +0900

    Update and rename bs2.py to BeautifulSoup3.py
    주석 수정 및 파일 이름 변경

--- BeautifulSoup3.py
+++ BeautifulSoup3.py
@@ -1,26 +1,26 @@
+# 텍스트 추출 및 정제
+
 from bs4 import BeautifulSoup
 
 html_doc = """
 <p class="price_color"> £51.77 </p>
 <h1> 상품 상세 정보 <br> 새로운 기능! </h1>
 """
-
 soup = BeautifulSoup(html_doc,'html.parser')
 
 # 가격 추출 및 정제
 price_tag = soup.find('p',class_='price_color')
-
 # 전달 받은 문자열 앞뒤 공백 제거
 price_text = price_tag.get_text(strip=True) #str 함수 적용하기 위해 .text 아닌 get_text함수 사용 
-
-
-#문자열 사이에 숫자가 아닌 문자가 같이 있을 때 데이터 정제
+#문자열+숫자 조합일 때 숫자만 남기기
 clean_price = float(price_text.replace('£',''))
-print(clean_price)
 
 # 제목 텍스트 추출 및 정제
-# .text를 사용할 경우는 자식태그의 텍스트를 포함 (현재 텍스트 사이 줄바꿈<br>태그 존재)
+# .text는 자식태그의 텍스트도 포함 (현재 텍스트 사이 줄바꿈<br>태그 존재)
 title_raw = soup.find('h1').text
-# get_text(strip)을 사용
+# .get_text(strip=True)는 모든 공백 제거 후 텍스트만 반환
 title_clean = soup.find('h1').get_text('',strip=True)
-print(title_clean)
+
+print(f"정제된 가격: {clean_price}")
+print(f"원시 제목: '{title_raw}'")
+print(f"정제된 제목: '{title_clean}'")
