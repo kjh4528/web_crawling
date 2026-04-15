32e7822673aa62ba421868894a311cca5cc02d56
kjh4528 <jeonghyeon.kimm@gmail.com>
2026-04-15 22:20:12 +0900

    Update and rename bs1.py to BeautifulSoup2.py
    주석 수정 및 파일 이름 변경

--- BeautifulSoup2.py
+++ BeautifulSoup2.py
@@ -1,3 +1,5 @@
+# 속성값 추출하기 
+
 from bs4 import BeautifulSoup
 
 html_doc = """
@@ -11,16 +13,16 @@ stock="10">
 
 soup = BeautifulSoup(html_doc,'html.parser')
 product_div = soup.find('div')
-link = soup.find('a')['href']
-link2 = soup.a['href']
-
+link = soup.find('a')['href'] #
+link2 = soup.a['href'] # 둘은 같은 결괏값 반환
 image = soup.find('img')
 
+# 딕셔너리처럼 접근
 product_id = product_div['id']
 item_classes = product_div['class']
 
-stock = product_div.get('data-stock') # get함수 쓰면 값이 없을 때 none 출력, 에러안뜨고 안전 
-
-missing_attr = product_div.get('data-price','N/A')
+# .get()으로 안전하게 접근 (없는 속성은 None 반환) -> 에러 없이 안전
+stock = product_div.get('data-stock') #'10'
+missing_attr = product_div.get('data-price','N/A')  # "N/A"
 img_alt = image.get('alt')
 
