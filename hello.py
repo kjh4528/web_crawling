661eeddb4554d28a2a70b8bc9bf319d246a063a9
김정현 <jeonghyeon.kimm@gmail.com>
2026-04-24 17:22:57 +0900

    Organize practice folders

--- hello.py
+++
@@ -1,9 +0,0 @@
-url = 'http://naver.com'
-#규칙1
-nurl = url.replace('http://','')
-#규칙2
-nurl = nurl.split('.')[0]
-print(nurl)
-#규칙3
-pw = nurl[:3] + str(len(nurl)) + str(nurl.count('e')) + '!'
-print(f'{url}의 비밀번호는 {pw} 입니다.')
